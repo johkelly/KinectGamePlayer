f965edb46fdbc7163794bb123bc2bc1740e57d38
johkelly <johkelly@mines.edu>
2014-10-29 20:55:13 -0600

    Remove Toolkit project, Add raw skeleton reader
    Raw skeleton reader uses an event delegate list to inform of new skeletons
    Tracks one user at a time, will attempt to acquire a new user if first
    one is lost.

--- KinectGamePlayer/RawSkeletonReader.cs
+++ KinectGamePlayer/RawSkeletonReader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Kinect;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,63 @@ namespace CSCI598.Proj3
 {
     class RawSkeletonReader
     {
+        private KinectSensor sensor;
+
+        public event EventHandler<Skeleton> rawSkeletonReady;
+        private int currentPlayerId;
+
+        public RawSkeletonReader()
+        {
+            foreach (var potentialSensor in KinectSensor.KinectSensors) {
+                if (potentialSensor.Status == KinectStatus.Connected) {
+                    sensor = potentialSensor;
+                }
+            }
+
+            if (null != sensor)
+            {
+                sensor.SkeletonStream.Enable();
+                sensor.SkeletonFrameReady += handleSkeletonFrame;
+            }
+        }
+
+        private void handleSkeletonFrame(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            if (rawSkeletonReady != null)
+            {
+                using (SkeletonFrame frame = e.OpenSkeletonFrame())
+                {
+                    if (frame == null)
+                    {
+                        return;
+                    }
+                    Skeleton[] skeletons = new Skeleton[frame.SkeletonArrayLength];
+                    try
+                    {
+                        frame.CopySkeletonDataTo(skeletons);
+                        // Find the skeleton of the player being tracked
+                        foreach (Skeleton s in skeletons)
+                        {
+                            if (currentPlayerId == -1 && s.TrackingId != -1)
+                            {
+                                currentPlayerId = s.TrackingId;
+                            }
+                            if (s.TrackingId == currentPlayerId)
+                            {
+                                rawSkeletonReady(this, s);
+                                return;
+                            }
+                        }
+                        // Did not see the current player ... drop tracking id
+                        currentPlayerId = -1;
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        // SkeletonFrame may throw when Kinect is in bad state
+                        // Ignore frame in that case
+                    }
+                }
+            }
+        }
     }
 }
