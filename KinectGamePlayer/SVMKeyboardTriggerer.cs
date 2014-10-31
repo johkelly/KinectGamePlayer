95bce77bf6ec9e0317ee890374fe4cb0d1662d46
Nathan <nthuff@gmail.com>
2014-10-30 21:07:01 -0600

    Stream Processing Step 1
    Reading in training data,
    On the fly processing of histogram data and passing into svm.

--- KinectGamePlayer/SVMKeyboardTriggerer.cs
+++ KinectGamePlayer/SVMKeyboardTriggerer.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LibSVMsharp;
+using LibSVMsharp.Helpers;
+using MathNet.Numerics.Statistics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,16 +16,51 @@ namespace CSCI598.Proj3
         public SVMKeyboardTriggerer(HistogramSharePoint hsp)
         {
             this.hsp = hsp;
+
         }
 
         public void run()
         {
+            List<Histogram> temp = null;
+            List<Histogram> hb;
             while (true)
             {
-                var hb = hsp.histBatch;
+               hb = hsp.histBatch;
                 System.Console.WriteLine((hb == null ? "null" : hb.ToString()));
                 Thread.Sleep(500);
             }
+            SVMProblem problem = SVMProblemHelper.Load(@"dataset_path.txt");
+
+            SVMParameter parameter = new SVMParameter();
+            parameter.Type = SVMType.C_SVC;
+            parameter.Kernel = SVMKernelType.RBF;
+            parameter.C = 1;
+            parameter.Gamma = 1;
+
+            SVMModel model = SVM.Train(problem, parameter);
+            while(true)
+            {
+                hb = hsp.histBatch;
+                if (temp != hb){
+                    temp = hb;
+                    int count = 1;
+                    List<SVMNode> nodes = new List<SVMNode>();
+                    for (int i = 1; i < temp.Count; i++)
+                    {
+                        Histogram histObject = temp[i];
+                        for (int j = 1; j < histObject.BucketCount; j++)
+                        {
+                            SVMNode node = new SVMNode();
+                            node.Index = count++;
+                            node.Value = histObject[j].Count;
+                            nodes.Add(node);
+                        }
+                    }
+                    double y = SVM.Predict(model, nodes.ToArray());
+                    System.Console.WriteLine(y);
+                }
+            }
+
         }
     }
 }
