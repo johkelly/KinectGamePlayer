72c897d81311c5de4a63c601bbbb46f574f2b131
johkelly <johkelly@mines.edu>
2014-10-30 22:01:04 -0600

    Add histogrammer library, test driver program

---
+++ Histogrammer/Point3D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Histogrammer
+{
+    class Point3D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+
+        internal Point3D difference(Point3D other)
+        {
+            return new Point3D {
+                X = this.X - other.X,
+                Y = this.Y - other.Y,
+                Z = this.Z - other.Z
+            };
+        }
+
+        internal double magnitude()
+        {
+            return Math.Sqrt(X * X + Y * Y + Z * Z);
+        }
+    }
+}
---
+++ KinectGamePlayer/HistogrammerRunner/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Histogrammer;
+using System.IO;
+using MathNet.Numerics.Statistics;
+
+namespace HistogrammerRunner
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            StreamWriter trainingFile = new StreamWriter("train.txt");
+            foreach (string iFile in Directory.GetFiles("data", "*.txt"))
+            {
+                System.Console.WriteLine(iFile);
+                List<Histogram> hgrams = Histogrammer.KinectInstanceHistogrammer.HJPDHistogram(iFile, -.001, 3, 10);
+                // to file
+                int c = 0;
+                if (iFile.Contains("Default")) {
+                    c = 0;
+                }
+                else if (iFile.Contains("Left"))
+                {
+                    c = 1;
+                }
+                else if (iFile.Contains("Right"))
+                {
+                    c = 2;
+                }
+                else if (iFile.Contains("LHip"))
+                {
+                    c = 3;
+                }
+                else if (iFile.Contains("RHip"))
+                {
+                    c = 4;
+                }
+                else if (iFile.Contains("Push"))
+                {
+                    c = 5;
+                }
+                StringBuilder builder = new StringBuilder();
+                int currIdx = 1;
+                foreach (Histogram h in hgrams)
+                {
+                    for (int i = 0; i < h.BucketCount; ++i) {
+                        builder.Append(currIdx.ToString() + ":" + (h[i].Count + " "));
+                        currIdx++;
+                    }
+                }
+                trainingFile.WriteLine(c + " " + builder.ToString());
+            }
+            System.Console.ReadKey(true);
+        }
+    }
+}
