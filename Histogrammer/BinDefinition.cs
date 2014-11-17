cb250a26bbf67a7867037871be9b389a065f193b
johkelly <johkelly@mines.edu>
2014-11-17 13:15:58 -0700

    Add necessary BinDefinition class

---
+++ Histogrammer/BinDefinition.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCI598.Proj3.Histogrammer
+{
+    public class BinDefinition
+    {
+        public double lowerBound { get; set; }
+        public double upperBound { get; set; }
+        public int numBins { get; set; }
+        public BinDefinition() {
+            lowerBound = double.MaxValue;
+            upperBound = double.MinValue;
+        }
+    }
+}
